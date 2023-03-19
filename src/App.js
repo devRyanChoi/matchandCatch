4f36e1f326484da2161fa7187725632f1aced972
mingun choi <ryanchoi100826@gmail.com>
2023-03-19 16:34:32 -0400

    modifid

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
 import React from 'react';
-import {Switch, Route} from "react-router-dom";
+
+
 import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
-import Home from "./components/Navbar"
 
 const App = () => (
-  return(
-    <>
-    <Route exact path="/" component={Home}/>
-    <Route exact path="/" component={Home}/>
-    <Route exact path="/" component={Home}/>
-    <Route exact path="/" component={Home}/>
-    <Route exact path="/" component={Home}/>
-
-    </>
-  )
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
 );
 
 export default App;
-{/* <div>
-<Navbar />
-<Header />
-<AboutUs />
-<SpecialMenu />
-<Chef />
-<Intro />
-<Laurels />
-<Gallery />
-<FindUs />
-<Footer />
-</div> */}

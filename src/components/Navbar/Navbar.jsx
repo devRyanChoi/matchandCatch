349ac92184f7c1e339a2d3cf98e8069b9d1982a9
mingun choi <ryanchoi100826@gmail.com>
2023-03-19 16:26:16 -0400

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+
+
+import images from '../../constants/images';
+import './Navbar.css';
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return(
+  <nav className="app_navbar">
+    <div className='app_navbar-logo'>
+      <img src={images.mc} alt="app logo" />
+      </div>
+      <ul className='app_navbar-links'>
+        <li className="p__opensans"><a href="#home">Past auction</a></li>
+        <li className="p__opensans"><a href="#home">Dealer offer</a></li>
+        <li className="p__opensans"><a href="#home">Leaderboard</a></li>
+      </ul>
+      <div className="app__navbar-login">
+        <a href="/" className="p__opensans">My Auction</a>
+        <div/>
+        <a href="#" className='p__opensans'>Sign In</a>
+      </div>
+      <div className="app__navbar-samllscreen">
+        <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => setToggleMenu(true)} />
+
+        {toggleMenu && (
+          <div className='app__navbar-smallscreen_overlay flex__center slide-bottm'>
+            <AiFillCloseCircle fontsize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
+            <ul className='app__navbar-smallscreen_links'>
+              <li className="p__opensans"><a href="#home">Past auction</a></li>
+              <li className="p__opensans"><a href="#home">Dealer offer</a></li>
+              <li className="p__opensans"><a href="#home">Leaderboard</a></li>
+            </ul>
+          </div>
+        )}
+
+
+      </div>
+  </nav>
+  )
+};
+  
+
+export default Navbar;
---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import {images} from '../../constants';
+import './Header.css';
+
+const Header = () => (
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title="We make selling your car easy, fast, and fair."/>
+      <h1 className='app__header-h1'>Are you looking to sell your car?</h1>
+      <p className='p__opensans' styLe={{margin: '2rem 0'}}></p>
+      <button type="button" className='custom__button'>Sell your car</button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.car1} alt="header img" />
+    </div>
+  </div>
+);
+
+export default Header;
